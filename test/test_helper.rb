3386e1cbdc8f095a0aead09ff8b435e13b661c34
Ara Hacopian <ara@changebot.ai>
2025-04-18 23:30:20 +0200

    Suppress warnings from mail gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,19 @@ require 'minitest/autorun'
 require 'rails'
 require 'action_mailer'
 require 'bento_actionmailer'
+
+# Suppress specific warnings from the mail gem parsers
+module Warning
+  class << self
+    alias_method :original_warn, :warn
+    # Suppress Mail gem parser 'assigned but unused variable - testEof' warnings
+    def warn(message)
+      if message =~ %r{/mail/parsers/.*: warning: assigned but unused variable - testEof}
+        return
+      end
+      original_warn(message)
+    end
+  end
+end
+
 require 'mail'
